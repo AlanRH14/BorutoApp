aa895c842d5ec940f5de31cd9309cd5f3359623f
Alan Andres Rosas Hernandez <alanarh.14@gmail.com>
2023-11-21 01:14:35 -0600

    Configuration of libraries and plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    kotlin("plugin.serialization") version embeddedKotlinVersion
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.aarh.borutoapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.aarh.borutoapp"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -25,16 +28,16 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -66,4 +69,59 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+
+    // Room Components
+    implementation("androidx.room:room-runtime:2.5.0")
+    ksp("androidx.room:room-compiler:2.5.0")
+    implementation("androidx.room:room-ktx:2.5.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+
+    // Paging 3.0
+    implementation("androidx.paging:paging-compose:3.3.0-alpha02")
+
+    // KotlinX Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
+
+    // DataStore Preferences
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    // Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:2.47")
+    ksp("com.google.dagger:hilt-android-compiler:2.47")
+    ksp("androidx.hilt:hilt-compiler:1.1.0-alpha01")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+     // Horizontal Pager and Indicators - Accompanist
+     implementation("com.google.accompanist:accompanist-pager:0.33.2-alpha")
+     implementation("com.google.accompanist:accompanist-pager-indicators:0.33.2-alpha")
+
+     // Swipe to Refresh - Accompanist
+     implementation("com.google.accompanist:accompanist-swiperefresh:0.33.2-alpha")
+
+     // System UI Controller - Accompanist
+     implementation("com.google.accompanist:accompanist-systemuicontroller:0.33.2-alpha")
+
+     // Palette API
+     implementation("androidx.palette:palette-ktx:1.0.0")
+
+     // Testing
+     androidTestImplementation("androidx.test:runner:1.5.2")
+     androidTestImplementation("androidx.test:rules:1.5.0")
+     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
+     debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
+
+     testImplementation("junit:junit:4.13.2")
+     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.20")
+     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
 }
