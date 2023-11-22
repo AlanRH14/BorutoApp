ff05dd781cf34676f1488ae4574cbc1fd0aa72ea
Alan Andres Rosas Hernandez <alanarh.14@gmail.com>
2023-11-22 12:56:32 -0600

    Setup Dagger Hilt

---
+++ app/src/main/java/com/aarh/borutoapp/di/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.aarh.borutoapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.aarh.borutoapp.data.local.BorutoDatabase
+import com.aarh.borutoapp.util.Constants.BORUTO_DATABASE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun providerDatabase(
+        @ApplicationContext context: Context,
+    ) = Room.databaseBuilder(
+        context,
+        BorutoDatabase::class.java,
+        BORUTO_DATABASE,
+    ).build()
+}
--- app/src/main/java/com/aarh/borutoapp/util/Constants.kt
+++ app/src/main/java/com/aarh/borutoapp/util/Constants.kt
@@ -2,6 +2,7 @@ package com.aarh.borutoapp.util
 
 object Constants {
     const val DETAILS_ARGUMENT_KEY = "heroId"
+    const val BORUTO_DATABASE = "boruto_database"
     const val HERO_DATABASE_TABLE = "hero_table"
     const val HERO_REMOTE_KEY_TABLE = "hero_remote_key_table"
 }
