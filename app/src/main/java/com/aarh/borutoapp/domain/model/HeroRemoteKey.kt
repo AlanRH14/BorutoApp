325e9302ea18256ce682f2c690738e0a73d0e662
Alan Andres Rosas Hernandez <alanarh.14@gmail.com>
2023-11-22 02:59:46 -0600

    Create Database converter class

--- app/src/main/java/com/aarh/borutoapp/domain/model/HeroRemoteKey.kt
+++ app/src/main/java/com/aarh/borutoapp/domain/model/HeroRemoteKey.kt
@@ -8,6 +8,6 @@ import com.aarh.borutoapp.util.Constants.HERO_REMOTE_KEY_TABLE
 data class HeroRemoteKey(
     @PrimaryKey(autoGenerate = false)
     val id: Int,
-    val prevKey: Int?,
-    val nextKey: Int?,
+    val prevPage: Int?,
+    val nextPage: Int?,
 )
