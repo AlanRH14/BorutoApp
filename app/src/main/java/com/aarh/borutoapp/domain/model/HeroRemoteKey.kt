0d4eda7a93e7cf75c0caad326f00cf28e2d630d7
Alan Andres Rosas Hernandez <alanarh.14@gmail.com>
2023-11-22 02:03:38 -0600

    Create Hero Remote Key Entity & Dao

---
+++ app/src/main/java/com/aarh/borutoapp/domain/model/HeroRemoteKey.kt
@@ -0,0 +1,13 @@
+package com.aarh.borutoapp.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.aarh.borutoapp.util.Constants.HERO_REMOTE_KEY_TABLE
+
+@Entity(tableName = HERO_REMOTE_KEY_TABLE)
+data class HeroRemoteKey(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val prevKey: Int?,
+    val nextKey: Int?,
+)
