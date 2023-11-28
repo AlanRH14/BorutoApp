d4111ae177fb46d97ed2e86ddeea86d51458d9a9
Alan Andres Rosas Hernandez <alanarh.14@gmail.com>
2023-11-27 20:56:05 -0600

    Create ApiResponse & BorutoApi

---
+++ app/src/main/java/com/aarh/borutoapp/domain/model/ApiResponse.kt
@@ -0,0 +1,13 @@
+package com.aarh.borutoapp.domain.model
+
+import com.aarh.borutoapp.domain.entity.Hero
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ApiResponse(
+    val success: Boolean,
+    val message: String? = null,
+    val prevPage: Int? = null,
+    val nextPage: Int? = null,
+    val heroes: List<Hero> = emptyList(),
+)
