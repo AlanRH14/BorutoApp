7e88f426a9a5b6a532ab23e3c251f0a0894e976f
Alan Andres Rosas Hernandez <alanarh.14@gmail.com>
2023-11-27 20:18:47 -0600

    Home Screen - Design Top Bar

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,16 +1,17 @@
 package com.aarh.borutoapp.presentation.screens.home
 
-import android.annotation.SuppressLint
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import com.aarh.borutoapp.presentation.screens.home.components.HomeTopBar
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@ExperimentalMaterial3Api
 @Composable
 fun HomeScreen() {
     Scaffold(
         topBar = {
             HomeTopBar {}
         },
+
     ) { paddingValues -> }
 }
