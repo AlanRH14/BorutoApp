807ba91fb3b324b0085ca6ef417d65ead4d18de9
Alan Andres Rosas Hernandez <alanarh.14@gmail.com>
2023-12-01 12:45:28 -0600

    Create RemoteDataSource & Implementation

--- app/src/main/java/com/aarh/borutoapp/data/repository/Repository.kt
+++ app/src/main/java/com/aarh/borutoapp/data/repository/Repository.kt
@@ -1,12 +1,20 @@
 package com.aarh.borutoapp.data.repository
 
+import androidx.paging.PagingData
+import com.aarh.borutoapp.domain.entity.Hero
 import com.aarh.borutoapp.domain.repository.DataStoreOperations
+import com.aarh.borutoapp.domain.repository.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class Repository @Inject constructor(
+    private val remote: RemoteDataSource,
     private val dataStore: DataStoreOperations,
 ) {
+
+    fun getAllHeroes(): Flow<PagingData<Hero>> {
+        return remote.getAllHeroes()
+    }
 
     suspend fun saveOnBoardingState(completed: Boolean) {
         dataStore.saveOnBoardingState(completed = completed)
--- app/src/main/java/com/aarh/borutoapp/util/Constants.kt
+++ app/src/main/java/com/aarh/borutoapp/util/Constants.kt
@@ -24,4 +24,6 @@ object Constants {
     const val PREFERENCES_KEY = "on_boarding_completed"
 
     const val BASE_URL = "http://0.0.0.0:8080"
+
+    const val ITEMS_PER_PAGE = 3
 }
