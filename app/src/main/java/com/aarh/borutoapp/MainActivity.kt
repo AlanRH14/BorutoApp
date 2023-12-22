b3bb44fe26a0f028831619989ae93539c54066de
Alan Andres Rosas Hernández <84395371+AlanRH14@users.noreply.github.com>
2023-12-22 16:36:43 -0600

    Revert " Design RatingWidget - FilledStar"

--- app/src/main/java/com/aarh/borutoapp/MainActivity.kt
+++ app/src/main/java/com/aarh/borutoapp/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.aarh.borutoapp.navigation.SetupNavGraph
 import com.aarh.borutoapp.ui.theme.BorutoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
-@ExperimentalMaterial3Api
 @ExperimentalFoundationApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
