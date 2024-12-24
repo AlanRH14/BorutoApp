c8f8345fca0ec77ee5d30b823bbc7e6eb482e831
Alan Andres Rosas Hernández <alanarh.14@gmail.com>
2024-12-24 13:05:53 -0600

    Change localhost root and logic

--- app/src/main/java/com/aarh/borutoapp/util/TimeUtils.kt
+++ app/src/main/java/com/aarh/borutoapp/util/TimeUtils.kt
@@ -2,12 +2,12 @@ package com.aarh.borutoapp.util
 
 fun getInitialRefreshData(
     lastUpdated: Long,
-    cacheTimeout: Int = 5,
+    cacheTimeout: Int = 1440,
 ): Boolean {
     val currentTime = System.currentTimeMillis()
     val diffInMinutes = (currentTime - lastUpdated) / 1000 / 60
 
-    return (diffInMinutes <= cacheTimeout)
+    return (diffInMinutes.toInt() <= cacheTimeout)
 }
 
 /*fun parseMillis(
