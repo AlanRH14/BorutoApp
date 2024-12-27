5af9042fad41dd6c0e56be1a64861f8bfdebd91c
Alan Andres Rosas Hernández <alanarh.14@gmail.com>
2024-12-26 20:56:09 -0600

    Change kotlin catalogs gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.serialization)
@@ -64,86 +65,21 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
     ksp(libs.hilt.android.compiler)
     implementation(libs.hilt.androidx.navigation.compose)
     implementation(libs.hilt.android)
-
+    ksp(libs.room.compiler)
     implementation(libs.room.android)
     annotationProcessor(libs.room.compiler)
     implementation(libs.room.paging.android)
-    ksp(libs.room.compiler)
-
-
     implementation(libs.retrofit)
     implementation(libs.okhttp)
-
     implementation(libs.kotlin.serialization)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.retrofit.converter.kotlinx.serialization)
-
     implementation(libs.coil.compose)
-
     implementation(libs.androidx.palette.ktx)
     implementation(libs.paging)
-
     implementation(libs.data.store)
-
     implementation(libs.horizontal.pager)
-
-
-    /* // Compose Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.5")
-
-    // Room Components
-    implementation("androidx.room:room-runtime:2.5.0")
-    ksp("androidx.room:room-compiler:2.5.0")
-    implementation("androidx.room:room-ktx:2.5.0")
-    implementation("androidx.room:room-paging:2.5.0")
-
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-
-    // Paging 3.0
-    implementation("androidx.paging:paging-compose:3.3.0-alpha02")
-
-    // KotlinX Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
-
-    // DataStore Preferences
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-    // Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.47")
-    ksp("com.google.dagger:hilt-android-compiler:2.47")
-    ksp("androidx.hilt:hilt-compiler:1.1.0-alpha01")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
-
-    // Coil
-    implementation("io.coil-kt:coil-compose:2.4.0")
-
-    // Horizontal Pager and Indicators - Accompanist
-    implementation("com.google.accompanist:accompanist-pager-indicators:0.33.2-alpha")
-
-    // Swipe to Refresh - Accompanist
-    implementation("com.google.accompanist:accompanist-swiperefresh:0.33.2-alpha")
-
-    // System UI Controller - Accompanist
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.33.2-alpha")
-
-    // Palette API
-    implementation("androidx.palette:palette-ktx:1.0.0")
-
-    // Testing
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
-
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.20")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1") */
 }
