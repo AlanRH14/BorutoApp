7edc96d68d9a16994c9c5f304f4193e538043c66
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2025-01-02 17:03:00 -0600

    Add constants parse error message.

--- app/src/main/java/com/aarh/borutoapp/util/error/EmptyScreen.kt
+++ app/src/main/java/com/aarh/borutoapp/util/error/EmptyScreen.kt
@@ -1,23 +1,76 @@
 package com.aarh.borutoapp.util.error
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.paging.LoadState
+import com.aarh.borutoapp.R
+import com.aarh.borutoapp.ui.theme.GraySystemUIColor
+import com.aarh.borutoapp.ui.theme.NETWORK_ERROR_ICON_HEIGHT
+import com.aarh.borutoapp.ui.theme.SMALL_PADDING
 import com.aarh.borutoapp.util.Constants.CONNECTION_EXCEPTION
 import com.aarh.borutoapp.util.Constants.INTERNET_UNAVAILABLE
 import com.aarh.borutoapp.util.Constants.SERVER_UNAVAILABLE
 import com.aarh.borutoapp.util.Constants.SOCKET_TIME_OUT_EXCEPTION
 import com.aarh.borutoapp.util.Constants.UNKNOWN_ERROR
+import io.ktor.client.network.sockets.SocketTimeoutException
 
 @Composable
 fun EmptyScreen(
-    error : LoadState.Error
+    error: LoadState.Error
 ) {
     val message by remember {
-        mutableStateOf("")
+        mutableStateOf(parseErrorMessage(error.toString()))
     }
+    val icon by remember {
+        mutableIntStateOf(R.drawable.ic_network_error)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            modifier = Modifier.size(NETWORK_ERROR_ICON_HEIGHT),
+            painter = painterResource(icon),
+            contentDescription = stringResource(R.string.network_error_icon),
+            tint = GraySystemUIColor
+        )
+        Text(
+            modifier = Modifier
+                .padding(top = SMALL_PADDING),
+            text = message,
+            color = GraySystemUIColor,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Medium,
+            fontSize = MaterialTheme.typography.titleLarge.fontSize
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun EmptyScreenPreview() {
+    EmptyScreen(error = LoadState.Error(SocketTimeoutException()))
 }
 
 fun parseErrorMessage(message: String): String {
