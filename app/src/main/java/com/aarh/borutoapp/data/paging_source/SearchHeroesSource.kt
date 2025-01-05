f0abfaeff296f6d81347e1c0d887c2200185c71d
Alan Andres Rosas Hernández <alanarh.14@gmail.com>
2025-01-04 22:00:38 -0600

    Setting Search Heroes Source

--- app/src/main/java/com/aarh/borutoapp/data/paging_source/SearchHeroesSource.kt
+++ app/src/main/java/com/aarh/borutoapp/data/paging_source/SearchHeroesSource.kt
@@ -9,10 +9,28 @@ import javax.inject.Inject
 class SearchHeroesSource @Inject constructor(
     private val borutoApi: BorutoApi,
     private val query: String
-): PagingSource<Int, Hero>() {
+) : PagingSource<Int, Hero>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Hero> {
-        TODO("Not yet implemented")
+        return try {
+            val apiResponse = borutoApi.searchHeroes(name = query)
+            val heroes = apiResponse.heroes
+            if (heroes.isNotEmpty()) {
+                LoadResult.Page(
+                    data = heroes,
+                    prevKey = apiResponse.prevPage,
+                    nextKey = apiResponse.nextPage,
+                )
+            } else {
+                LoadResult.Page(
+                    data = emptyList(),
+                    prevKey = null,
+                    nextKey = null
+                )
+            }
+        } catch (e: Exception) {
+
+        }
     }
 
     override fun getRefreshKey(state: PagingState<Int, Hero>): Int? {
