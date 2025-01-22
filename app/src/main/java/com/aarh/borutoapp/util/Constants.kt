df273a183e85f2cdd7944cc1249d8ddb37873707
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-01-22 10:57:58 -0600

    Setting deploy server url

--- app/src/main/java/com/aarh/borutoapp/util/Constants.kt
+++ app/src/main/java/com/aarh/borutoapp/util/Constants.kt
@@ -24,7 +24,8 @@ object Constants {
     const val PREFERENCES_NAME = "boruto_preferences"
     const val PREFERENCES_KEY = "on_boarding_completed"
 
-    const val BASE_URL = "http://10.0.2.2:8080"
+    //const val BASE_URL = "http://10.0.2.2:8080"
+    const val BASE_URL = "https://boruto-server-udemy-eb3fb2f5057f.herokuapp.com"
 
     const val ITEMS_PER_PAGE = 3
 
