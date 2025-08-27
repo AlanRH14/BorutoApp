7bf7c0c988ac9c7045472e3d3ac3711f699cd3cc
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-27 17:14:08 -0600

    Change Injection config part. 20.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/home/HomeViewModel.kt
@@ -2,11 +2,8 @@ package com.aarh.borutoapp.presentation.screens.home
 
 import androidx.lifecycle.ViewModel
 import com.aarh.borutoapp.domain.use_case.UseCases
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 
-@HiltViewModel
-class HomeViewModel @Inject constructor(
+class HomeViewModel(
     private val useCases: UseCases,
 ) : ViewModel() {
     val getAllHeroes = useCases.getAllHeroesUseCase()
