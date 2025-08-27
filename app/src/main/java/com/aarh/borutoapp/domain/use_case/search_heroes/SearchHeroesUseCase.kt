408382a595bed5d9b80fbee9cd3b14fb97b049e1
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-27 17:06:42 -0600

    Change Injection config part. 17.

--- app/src/main/java/com/aarh/borutoapp/domain/use_case/search_heroes/SearchHeroesUseCase.kt
+++ app/src/main/java/com/aarh/borutoapp/domain/use_case/search_heroes/SearchHeroesUseCase.kt
@@ -1,12 +1,12 @@
 package com.aarh.borutoapp.domain.use_case.search_heroes
 
 import androidx.paging.PagingData
-import com.aarh.borutoapp.data.repository.Repository
 import com.aarh.borutoapp.domain.entity.Hero
+import com.aarh.borutoapp.domain.repository.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
 
 class SearchHeroesUseCase(
-    private val repository: Repository
+    private val repository: RemoteDataSource
 ) {
     operator fun invoke(query: String): Flow<PagingData<Hero>> {
         return repository.searchHeroes(query = query)
