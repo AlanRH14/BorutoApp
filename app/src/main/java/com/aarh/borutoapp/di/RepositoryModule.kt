e50a7ad69f93ca1745d48e48cbd953b7b7993980
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-27 13:10:45 -0600

    Change hilt to koin part. 16.

--- app/src/main/java/com/aarh/borutoapp/di/RepositoryModule.kt
+++ app/src/main/java/com/aarh/borutoapp/di/RepositoryModule.kt
@@ -1,22 +1,12 @@
 package com.aarh.borutoapp.di
 
-import android.content.Context
-import com.aarh.borutoapp.data.repository.DataStoreOperationsImpl
-import com.aarh.borutoapp.data.repository.Repository
-import com.aarh.borutoapp.domain.repository.DataStoreOperations
 import com.aarh.borutoapp.domain.use_case.UseCases
 import com.aarh.borutoapp.domain.use_case.get_all_heroes.GetAllHeroesUseCase
 import com.aarh.borutoapp.domain.use_case.get_selected_hero.GetSelectedHeroUseCase
 import com.aarh.borutoapp.domain.use_case.read_onboarding.ReadOnBoardingUseCase
 import com.aarh.borutoapp.domain.use_case.save_onboarding.SaveOnBoardingUseCase
 import com.aarh.borutoapp.domain.use_case.search_heroes.SearchHeroesUseCase
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
 import org.koin.dsl.module
-import javax.inject.Singleton
 
 val repositoryModule = module {
 
