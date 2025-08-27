96529e89eedbc29d41ccfa7816438d1e29e48ccb
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-27 14:02:29 -0600

    Change config for DataStore part. 5.

--- app/src/main/java/com/aarh/borutoapp/data/repository/DataStoreHandleImpl.kt
+++ app/src/main/java/com/aarh/borutoapp/data/repository/DataStoreHandleImpl.kt
@@ -4,6 +4,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
+import com.aarh.borutoapp.common.PreferencesKey
 import com.aarh.borutoapp.domain.repository.DataStoreHandle
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -14,23 +15,21 @@ class DataStoreHandleImpl(
     private val dataStore: DataStore<Preferences>
 ) : DataStoreHandle {
 
-    override suspend fun saveState(completed: Boolean) {
+    override suspend fun <T> saveState(key: PreferencesKey<T>, value: T) {
         dataStore.edit { preferences ->
-            preferences[PreferencesKey.onBoardingKey] = completed
+            preferences[key.preferencesKey] = value
         }
     }
 
-    override fun readState(): Flow<Boolean> {
-        return dataStore.data
-            .catch { exception ->
-                if (exception is IOException) {
-                    emit(emptyPreferences())
-                } else {
-                    throw exception
-                }
-            }.map { preferences ->
-                val onBoardingState = preferences[PreferencesKey.onBoardingKey] ?: false
-                onBoardingState
+    override fun <T> readState(key: PreferencesKey<T>): Flow<T> {
+        return dataStore.data.catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw Exception()
             }
+        }.map { preferences ->
+            preferences[key.preferencesKey] ?: key.default
+        }
     }
 }
