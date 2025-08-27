2eac27ee251eefcf631f31e1fe50b99234533819
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-27 16:10:12 -0600

    Change config for DataStore part. 8.

--- app/src/main/java/com/aarh/borutoapp/data/repository/LocalDataSourceImpl.kt
+++ app/src/main/java/com/aarh/borutoapp/data/repository/LocalDataSourceImpl.kt
@@ -1,15 +1,14 @@
 package com.aarh.borutoapp.data.repository
 
-import com.aarh.borutoapp.data.local.BorutoDatabase
+import com.aarh.borutoapp.data.local.dao.HeroDao
 import com.aarh.borutoapp.domain.entity.Hero
 import com.aarh.borutoapp.domain.repository.LocalDataSource
 
 class LocalDataSourceImpl(
-    borutoDatabase: BorutoDatabase
+    private val dao: HeroDao
 ) : LocalDataSource {
-    private val heroDao = borutoDatabase.heroDao()
 
     override suspend fun getSelected(heroId: Int): Hero {
-        return heroDao.getSelectedHero(heroId = heroId)
+        return dao.getSelectedHero(heroId = heroId)
     }
 }
