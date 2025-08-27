f54a0753c764a42f22366dd8f75648970a33c57d
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-27 13:59:05 -0600

    Change config for DataStore part. 4.

--- app/src/main/java/com/aarh/borutoapp/domain/repository/DataStoreHandle.kt
+++ app/src/main/java/com/aarh/borutoapp/domain/repository/DataStoreHandle.kt
@@ -1,8 +1,9 @@
 package com.aarh.borutoapp.domain.repository
 
+import com.aarh.borutoapp.common.PreferencesKey
 import kotlinx.coroutines.flow.Flow
 
 interface DataStoreHandle {
     suspend fun <T> saveState(key: PreferencesKey<T>, value: T)
-    fun <T> readState(): Flow<T>
+    fun <T> readState(key: PreferencesKey<T>): Flow<T>
 }
