1a2fc61de040c30568fb8cf27c3f9b57494e962e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-27 13:16:59 -0600

    Change hilt to koin part. 18.

--- app/src/main/java/com/aarh/borutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/aarh/borutoapp/data/local/BorutoDatabase.kt
@@ -1,15 +1,12 @@
 package com.aarh.borutoapp.data.local
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.aarh.borutoapp.data.local.dao.HeroDao
 import com.aarh.borutoapp.data.local.dao.HeroRemoteKeysDao
 import com.aarh.borutoapp.domain.entity.Hero
 import com.aarh.borutoapp.domain.entity.HeroRemoteKeys
-import com.aarh.borutoapp.util.Constants.BORUTO_DATABASE_TEST
 
 @Database(entities = [Hero::class, HeroRemoteKeys::class], version = 1)
 @TypeConverters(DataBaseConverter::class)
