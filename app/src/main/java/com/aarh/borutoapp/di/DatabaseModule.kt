c15880d037ae80a3d430a0b866440d1b914a3865
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-27 10:06:46 -0600

    Change hilt to koin part. 1.

--- app/src/main/java/com/aarh/borutoapp/di/DatabaseModule.kt
+++ app/src/main/java/com/aarh/borutoapp/di/DatabaseModule.kt
@@ -1,41 +1,21 @@
 package com.aarh.borutoapp.di
 
-import android.content.Context
 import androidx.room.Room
 import com.aarh.borutoapp.data.local.BorutoDatabase
 import com.aarh.borutoapp.data.repository.LocalDataSourceImpl
 import com.aarh.borutoapp.domain.repository.LocalDataSource
 import com.aarh.borutoapp.util.Constants.BORUTO_DATABASE
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
 
-@Module
-@InstallIn(SingletonComponent::class)
-object DatabaseModule {
-
-    @Provides
-    @Singleton
-    fun providerDatabase(
-        @ApplicationContext context: Context,
-    ): BorutoDatabase {
-        return Room.databaseBuilder(
-            context,
+val databaseModule = module {
+    single {
+        Room.databaseBuilder(
+            androidContext(),
             BorutoDatabase::class.java,
             BORUTO_DATABASE,
         ).build()
     }
 
-    @Provides
-    @Singleton
-    fun providesLocalDataSource(
-        database: BorutoDatabase
-    ): LocalDataSource {
-        return LocalDataSourceImpl(
-            borutoDatabase = database
-        )
-    }
+    single { LocalDataSourceImpl(borutoDatabase = get()) }
 }
