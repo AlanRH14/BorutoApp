4b745713cdbd995cdab8d8c894868b7c34a273a9
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-28 12:26:39 -0600

    Config MVI project part. 4.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/welcome/WelcomeViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aarh.borutoapp.domain.use_case.UseCases
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
@@ -15,7 +17,16 @@ class WelcomeViewModel(
     private val _state = MutableStateFlow(WelcomeState())
     val state = _state.asStateFlow()
 
-    fun saveOnBoardingState(completed: Boolean) {
+    private val _effect = MutableSharedFlow<WelcomeEffect>()
+    val effect = _effect.asSharedFlow()
+
+    fun onEvent(event: WelcomeUIEvent) {
+        when (event) {
+            is WelcomeUIEvent.OnSaveOnBoardingState -> saveOnBoardingState(completed = event.completed)
+        }
+    }
+
+    private fun saveOnBoardingState(completed: Boolean) {
         viewModelScope.launch(Dispatchers.IO) {
             useCases.saveOnBoardingUseCase(completed = completed)
         }
