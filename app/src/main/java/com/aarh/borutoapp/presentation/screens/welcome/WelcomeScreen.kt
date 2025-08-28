f85326251496e13c19fbf59435387e98303b808a
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-28 12:47:22 -0600

    Config MVI project part. 17.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/welcome/WelcomeScreen.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.aarh.borutoapp.navigation.Screen
@@ -29,8 +27,6 @@ fun WelcomeScreen(
     navController: NavHostController,
     welcomeViewModel: WelcomeViewModel = koinViewModel(),
 ) {
-
-    val state by welcomeViewModel.state.collectAsState()
 
     LaunchedEffect(key1 = true) {
         welcomeViewModel.effect.collectLatest {
