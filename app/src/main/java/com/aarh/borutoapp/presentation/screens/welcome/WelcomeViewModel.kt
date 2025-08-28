e14a5861d4d7f39ba898da95f937601327929b95
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-28 12:43:19 -0600

    Config MVI project part. 11.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/welcome/WelcomeViewModel.kt
@@ -5,9 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.aarh.borutoapp.domain.use_case.UseCases
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class WelcomeViewModel(
