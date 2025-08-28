1a4b51ba4789237e01b2b2b710e9494a1d9f13d8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-28 07:37:45 -0600

    Change Injection config part. 29.

--- app/src/main/java/com/aarh/borutoapp/MyApplication.kt
+++ app/src/main/java/com/aarh/borutoapp/MyApplication.kt
@@ -5,7 +5,10 @@ import com.aarh.borutoapp.di.databaseModule
 import com.aarh.borutoapp.di.networkModule
 import com.aarh.borutoapp.di.repositoryModule
 import dagger.hilt.android.HiltAndroidApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 @HiltAndroidApp
 class MyApplication : Application() {
@@ -13,6 +16,8 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidContext(this@MyApplication)
+            androidLogger(level = Level.DEBUG)
             modules(
                 networkModule,
                 databaseModule,
