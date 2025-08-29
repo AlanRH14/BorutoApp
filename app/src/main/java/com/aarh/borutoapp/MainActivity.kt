8d50c1513b243e600caf71005234959b63ec12af
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-29 12:46:54 -0600

    Change config splash part. 3.

--- app/src/main/java/com/aarh/borutoapp/MainActivity.kt
+++ app/src/main/java/com/aarh/borutoapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.aarh.borutoapp
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -9,6 +12,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.aarh.borutoapp.navigation.SetupNavGraph
 import com.aarh.borutoapp.ui.theme.BorutoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +24,29 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setOnExitAnimationListener { splashScreenView ->
+                val rotation = ObjectAnimator.ofFloat(
+                    splashScreenView.iconView,
+                    View.ROTATION,
+                    0F,
+                    360F
+                )
+
+                rotation.apply {
+                    duration = 1000
+                    interpolator = AccelerateDecelerateInterpolator()
+
+                    doOnEnd {
+                        splashScreenView.remove()
+                    }
+
+                    start()
+                }
+            }
+        }
+
         setContent {
             BorutoAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
