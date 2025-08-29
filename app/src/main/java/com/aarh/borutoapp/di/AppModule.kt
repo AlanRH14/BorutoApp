df7c4036725073654ff1a7196411d4d426839629
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-29 13:48:19 -0600

    Change config splash part. 9.

--- app/src/main/java/com/aarh/borutoapp/di/AppModule.kt
+++ app/src/main/java/com/aarh/borutoapp/di/AppModule.kt
@@ -3,7 +3,6 @@ package com.aarh.borutoapp.di
 import com.aarh.borutoapp.presentation.screens.detail.DetailsViewModel
 import com.aarh.borutoapp.presentation.screens.home.HomeViewModel
 import com.aarh.borutoapp.presentation.screens.search.SearchViewModel
-import com.aarh.borutoapp.presentation.screens.splash.SplashViewModel
 import com.aarh.borutoapp.presentation.screens.welcome.WelcomeViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
