a6aa35e82136c8c92749d1b9e6cff37ea42a4342
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-08 11:18:16 -0600

    Change config HomeViewModel part. 1.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/home/HomeViewModel.kt
@@ -1,10 +1,25 @@
 package com.aarh.borutoapp.presentation.screens.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.aarh.borutoapp.domain.use_case.UseCases
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val useCases: UseCases,
 ) : ViewModel() {
-    val getAllHeroes = useCases.getAllHeroesUseCase()
+
+    fun onEvent(event: T) {
+
+    }
+
+    private fun getAlHeroes() {
+        viewModelScope.launch(Dispatchers.IO) {
+            useCases.getAllHeroesUseCase().collect {
+
+            }
+        }
+    }
+
 }
