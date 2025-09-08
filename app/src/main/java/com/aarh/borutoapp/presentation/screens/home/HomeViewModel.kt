6a67439a7d4a44c8e7668996235387437b16e6b6
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-08 14:39:50 -0600

    Change config HomeViewModel part. 36.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.aarh.borutoapp.presentation.screens.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aarh.borutoapp.domain.use_case.UseCases
+import com.aarh.borutoapp.presentation.screens.home.components.HomeEffect
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
--- app/src/main/java/com/aarh/borutoapp/presentation/screens/home/components/HomeEffect.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/home/components/HomeEffect.kt
@@ -1,4 +1,4 @@
-package com.aarh.borutoapp.presentation.screens.home
+package com.aarh.borutoapp.presentation.screens.home.components
 
 sealed interface HomeEffect {
     data class NavigateToDetail(val heroID: Int) : HomeEffect
