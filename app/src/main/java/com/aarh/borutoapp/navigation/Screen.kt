501519965e25c9ae9d16a6b80c998b0df65aa523
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-08 13:36:37 -0600

    Change config HomeViewModel part. 10.

--- app/src/main/java/com/aarh/borutoapp/navigation/Screen.kt
+++ app/src/main/java/com/aarh/borutoapp/navigation/Screen.kt
@@ -5,12 +5,12 @@ import com.aarh.borutoapp.util.Constants.HOME_SCREEN_ROUTE
 import com.aarh.borutoapp.util.Constants.SEARCH_SCREEN_ROUTE
 import com.aarh.borutoapp.util.Constants.WELCOME_SCREEN_ROUTE
 
-sealed class Screen(val route: String) {
-    data object Welcome : Screen(route = WELCOME_SCREEN_ROUTE)
-    data object Home : Screen(route = HOME_SCREEN_ROUTE)
-    data object Details : Screen(route = "$DETAILS_SCREEN_ROUTE/{heroId}") {
-        fun passHeroId(heroId: Int) = "$DETAILS_SCREEN_ROUTE/$heroId"
-    }
+sealed interface Screen
 
-    data object Search : Screen(route = SEARCH_SCREEN_ROUTE)
+data object Welcome : Screen
+data object Home : Screen(route = HOME_SCREEN_ROUTE)
+data object Details : Screen(route = "$DETAILS_SCREEN_ROUTE/{heroId}") {
+    fun passHeroId(heroId: Int) = "$DETAILS_SCREEN_ROUTE/$heroId"
 }
+
+data object Search : Screen(route = SEARCH_SCREEN_ROUTE)
