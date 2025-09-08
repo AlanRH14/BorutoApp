ff6f42e21ebbe036e3c2abd878fcbc4b54f18066
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-08 13:43:39 -0600

    Change config HomeViewModel part. 14.

--- app/src/main/java/com/aarh/borutoapp/navigation/NavGraph.kt
+++ app/src/main/java/com/aarh/borutoapp/navigation/NavGraph.kt
@@ -3,16 +3,12 @@ package com.aarh.borutoapp.navigation
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.aarh.borutoapp.presentation.screens.detail.DetailScreen
 import com.aarh.borutoapp.presentation.screens.home.HomeScreen
 import com.aarh.borutoapp.presentation.screens.search.SearchScreen
 import com.aarh.borutoapp.presentation.screens.welcome.WelcomeScreen
-import com.aarh.borutoapp.util.Constants.DETAILS_ARGUMENT_KEY
 
 @ExperimentalFoundationApi
 @Composable
@@ -23,28 +19,23 @@ fun SetupNavGraph(
 
     NavHost(
         navController = navController,
-        startDestination = NavRoutes.Welcome.route,
+        startDestination = Welcome,
     ) {
-        composable(route = NavRoutes.Welcome.route) {
+        composable<Welcome> {
             WelcomeScreen(
                 modifier = modifier,
                 navController = navController
             )
         }
-        composable(route = NavRoutes.Home.route) {
+
+        composable<Home> {
             HomeScreen(navController = navController)
         }
-        composable(
-            route = NavRoutes.Details.route,
-            arguments = listOf(
-                navArgument(DETAILS_ARGUMENT_KEY) {
-                    type = NavType.IntType
-                },
-            ),
-        ) {
+
+        composable<Details> {
             DetailScreen(navController = navController)
         }
-        composable(route = NavRoutes.Search.route) {
+        composable<Search> {
             SearchScreen(navController = navController)
         }
     }
