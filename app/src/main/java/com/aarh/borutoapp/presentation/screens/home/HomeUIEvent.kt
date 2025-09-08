4cd68b5de09ae3b2c6852dfe552f15aa2808ccc1
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-08 14:33:32 -0600

    Change config HomeViewModel part. 33.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/home/HomeUIEvent.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/home/HomeUIEvent.kt
@@ -1,6 +1,7 @@
 package com.aarh.borutoapp.presentation.screens.home
 
 sealed interface HomeUIEvent {
-    data object OnGeAllHeroes: HomeUIEvent
-    data class OnClickHeroItem(val heroID: Int): HomeUIEvent
+    data object OnGeAllHeroes : HomeUIEvent
+    data class OnClickHeroItem(val heroID: Int) : HomeUIEvent
+    data object OnSearchClicked : HomeUIEvent
 }
