e13880b0b1584f22ded1d9fda8452524d4cf5768
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-08 17:09:10 -0600

    Change config DetailsViewModel part. 8.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/detail/DetailsState.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/detail/DetailsState.kt
@@ -1,10 +1,9 @@
 package com.aarh.borutoapp.presentation.screens.detail
 
-import androidx.compose.ui.graphics.Color
 import com.aarh.borutoapp.domain.entity.Hero
 
 data class DetailsState(
     val isLoading: Boolean = false,
     val selectedHero: Hero? = null,
-    val colorPalette: Map<String, Color> = emptyMap()
+    val colorPalette: Map<String, String> = emptyMap()
 )
