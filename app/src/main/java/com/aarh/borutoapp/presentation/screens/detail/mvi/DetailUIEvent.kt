bdcf0c9176bb995ef9d1ff0a07e618a2bfc656a3
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-10 16:34:31 -0600

    Change config Search module part. 27.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/detail/mvi/DetailUIEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.aarh.borutoapp.presentation.screens.detail.mvi
-
-sealed interface DetailUIEvent {
-    data class OnGetSelectedHero(val heroID: Int) : DetailUIEvent
-    data object OnGenerateColorPalette : DetailUIEvent
-    data class OnSetColorPalette(val colors: Map<String, String>): DetailUIEvent
-    data object OnBackClicked : DetailUIEvent
-}
