d36905ad347dd1f28b087ab9d399273b48e0c63e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-10 13:13:19 -0600

    Change config Search module part. 18.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/search/SearchUIEvent.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/search/SearchUIEvent.kt
@@ -3,5 +3,6 @@ package com.aarh.borutoapp.presentation.screens.search
 sealed interface SearchUIEvent {
     data class OnSearchClicked(val query: String) : SearchUIEvent
     data class OnUpdateSearchQuery(val query: String) : SearchUIEvent
-    data object OnCloseClicked: SearchUIEvent
+    data class OnHeroItemClicked(val heroID: Int) : SearchUIEvent
+    data object OnCloseClicked : SearchUIEvent
 }
