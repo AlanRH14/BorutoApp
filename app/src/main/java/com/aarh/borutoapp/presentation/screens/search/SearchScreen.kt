1666b7bbf4da6386bfd5168d6722856c95d72c51
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-10 13:28:54 -0600

    Change config Search module part. 22.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/search/SearchScreen.kt
@@ -3,14 +3,17 @@ package com.aarh.borutoapp.presentation.screens.search
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.aarh.borutoapp.navigation.Details
 import com.aarh.borutoapp.presentation.common.widgets.HeroesListContent
 import com.aarh.borutoapp.presentation.screens.home.mvi.HomeUIEvent
 import com.aarh.borutoapp.presentation.screens.search.components.SearchTopBar
+import kotlinx.coroutines.flow.collectLatest
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -20,6 +23,16 @@ fun SearchScreen(
 ) {
     val state by searchViewModel.state.collectAsStateWithLifecycle()
     val heroes = state.heroes.collectAsLazyPagingItems()
+
+    LaunchedEffect(key1 = true) {
+        searchViewModel.effect.collectLatest { effect ->
+            when (effect) {
+                is SearchEffect.NavigateToDetail -> navController.navigate(Details(heroID = effect.heroID))
+
+                is SearchEffect.NavigateToBack -> navController.popBackStack()
+            }
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -35,8 +48,10 @@ fun SearchScreen(
                 onEvent = {
                     when (it) {
                         is HomeUIEvent.OnHeroItemClicked -> {
-                            searchViewModel.onEvent(it.heroID)
+                            searchViewModel.onEvent(SearchUIEvent.OnHeroItemClicked(it.heroID))
                         }
+
+                        else -> Unit
                     }
                 }
             )
