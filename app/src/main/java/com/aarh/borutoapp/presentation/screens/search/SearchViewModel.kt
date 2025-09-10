588fba61ea68e30f955043b33ceefd84d3b571a4
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-10 12:45:04 -0600

    Change config Search module part. 10.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/search/SearchViewModel.kt
@@ -2,15 +2,11 @@ package com.aarh.borutoapp.presentation.screens.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import androidx.room.util.query
-import com.aarh.borutoapp.domain.entity.Hero
 import com.aarh.borutoapp.domain.use_case.UseCases
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -24,7 +20,6 @@ class SearchViewModel(
 
     private val _effect = MutableSharedFlow<SearchEffect>()
     val effect = _effect.asSharedFlow()
-
 
     private fun onEvent(event: SearchUIEvent) {
         when (event) {
