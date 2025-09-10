bd541c16793536ce7d406dbd904eb650886914f5
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-10 12:40:53 -0600

    Change config Search module part. 8.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/search/SearchState.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/search/SearchState.kt
@@ -2,9 +2,11 @@ package com.aarh.borutoapp.presentation.screens.search
 
 import androidx.paging.PagingData
 import com.aarh.borutoapp.domain.entity.Hero
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 data class SearchState(
     val isLoading: Boolean = false,
     val query: String = "",
-    val heroes: PagingData<Hero> = PagingData.empty(),
+    val heroes: Flow<PagingData<Hero>> = emptyFlow(),
 )
