d15f0c2d0021557c95c3d3c159fafb11f448ce91
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-10 10:27:43 -0600

    Change config Search module part. 3.

--- app/src/main/java/com/aarh/borutoapp/presentation/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/aarh/borutoapp/presentation/screens/search/SearchViewModel.kt
@@ -9,22 +9,24 @@ import com.aarh.borutoapp.domain.use_case.UseCases
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class SearchViewModel(
     private val useCases: UseCases,
 ) : ViewModel() {
-    private val _searchQuery = MutableStateFlow("")
-    val searchQuery: StateFlow<String> get() = _searchQuery
+    private val _state = MutableStateFlow(SearchState())
+    val state = _state.asStateFlow()
 
     private val _searchHeroes = MutableStateFlow<PagingData<Hero>>(PagingData.empty())
     val searchHeroes: StateFlow<PagingData<Hero>> get() = _searchHeroes
 
-    fun updateSearchQuery(query: String) {
-        _searchQuery.value = query
+    private fun updateSearchQuery(query: String) {
+        _state.update { it.copy(query = query) }
     }
 
-    fun searchHeroes(query: String) {
+    private fun searchHeroes(query: String) {
         viewModelScope.launch(Dispatchers.IO) {
             useCases.searchHeroesUseCase.invoke(query).cachedIn(viewModelScope).collect { hero ->
                 _searchHeroes.value = hero
