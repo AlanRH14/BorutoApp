c6ba720ea37b93c33dd5a8aa927c541c31432475
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2026-08-01 21:25:10 -0600

    Change color fumction deprecated in status bar part. 2

--- app/src/main/java/com/aarh/borutoapp/ui/theme/Theme.kt
+++ app/src/main/java/com/aarh/borutoapp/ui/theme/Theme.kt
@@ -1,8 +1,6 @@
 package com.aarh.borutoapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import androidx.activity.SystemBarStyle
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -10,13 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowCompat.enableEdgeToEdge
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple200,
