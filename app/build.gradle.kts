b9ef9bad9279fca5bd14dcf934d55481bd7caef0
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2026-08-02 19:11:02 -0600

    Change name lib splashscreen part. 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
 
     implementation(libs.androidx.navigation.compose)
 
-    implementation(libs. androdix.core.splashscreen)
+    implementation(libs. androidx.core.splashscreen)
 
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
